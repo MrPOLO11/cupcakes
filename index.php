de024def0e0d19f4213bdcc6a9c8318111df4244
Marcos Rivera <mrivera22@mail.greenriver.edu>
2020-01-13 13:02:58 -0500

    Created index page with simple implementation of values and objects. Need to implement associative array for cupcake flavors, then display them to page with flavors stored in array rather than html.

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>Cupcakes Assignment</title>
+</head>
+<body>
+    <h1><strong>Cupcake Fundraiser</strong></h1>
+    <div>
+        <legend>Your name: </legend>
+        <input type="text">
+    </div>
+
+    <div>
+        <legend>Cupcake Flavors: </legend>
+        <input type="checkbox" value="" id="" name="flavors[]">
+        <label for="">The Grasshopper</label>
+    </div>
+
+    <div>
+        <input type="checkbox" value="" id="" name="flavors[]">
+        <label for="">Chunky Monkey</label>
+    </div>
+
+    <div>
+        <input type="checkbox" value="" id="" name="flavors[]">
+        <label for="">Whiskey Maple Bacon</label>
+    </div>
+
+    <div>
+        <input type="checkbox" value="" id="" name="flavors[]">
+        <label for="">Carrot Walnut</label>
+    </div>
+
+    <div>
+        <input type="checkbox" value="" id="" name="flavors[]">
+        <label for="">Banana-palooza</label>
+    </div>
+
+    <div>
+        <input type="checkbox" value="" id="" name="flavors[]">
+        <label for="">Red Velvet</label>
+    </div>
+
+    <div>
+        <input type="checkbox" value="" id="" name="flavors[]">
+        <label for="">Lemon Drop</label>
+    </div>
+
+    <button id="submit" type="submit">
+        Order Up
+    </button>
+</body>
+</html>
